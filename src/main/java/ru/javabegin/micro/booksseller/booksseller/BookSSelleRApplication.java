c4b34fa0bf7404d943d622b314e3a92fb44fa648
kzNEKT0kz <gabdulmanaphasan6@gmail.com>
2025-08-12 21:50:29 +0600

    Create New Project

---
+++ src/main/java/ru/javabegin/micro/booksseller/booksseller/BookSSelleRApplication.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.booksseller.booksseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSSelleRApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSSelleRApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javabegin/micro/booksseller/booksseller/BookSSelleRApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.booksseller.booksseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSSelleRApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
