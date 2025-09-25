4b9d4b08619a796d8f378d8695f8ee5bcd317600
kzNEKT0kz <gabdulmanaphasan6@gmail.com>
2025-09-25 22:10:37 +0600

    Add SchemaRegistry

---
+++ SchemaRegistry/src/test/java/ru/javabegin/micro/booksseller/schemaregistry/SchemaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.booksseller.schemaregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
