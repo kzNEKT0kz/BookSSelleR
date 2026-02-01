641a24befab274474db3434803ba3fda598b1bc4
kzNEKT0kz <gabdulmanaphasan6@gmail.com>
2026-02-01 15:11:37 +0600

    Add Empty Search-Api

---
+++ search-api/src/main/java/ru/javabegin/micro/booksseller/searchapi/SearchApiApplication.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.booksseller.searchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApiApplication.class, args);
+    }
+
+}
---
+++ search-api/src/test/java/ru/javabegin/micro/booksseller/searchapi/SearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.booksseller.searchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
